1630e77ada4ae2019262d78de6edd7ad48442d2b
Paul Yu <pauldotyu@gmail.com>
2021-06-24 11:55:38 -0700

    removing ag from output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,6 +10,6 @@ output "acr_name" {
   value = azurerm_container_registry.cu.name
 }
 
-output "appgw_id" {
-  value = azurerm_application_gateway.ag.id
-}
+# output "appgw_id" {
+#   value = azurerm_application_gateway.ag.id
+# }
